211db4f936c6bf2fd796ed5efccd34a2b66b42ee
Shasha <shasha@runwithitsynthetics.com>
2020-06-07 19:22:30 -0600

    Number of Islands

--- medium/NumberofIsland.java
+++ medium/NumberofIsland.java
@@ -3,101 +3,79 @@ package leetcode.medium;
 import java.util.ArrayList;
 import java.util.HashMap;
 /*
- * Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
+ * Given a 2d grid map of '1's (land) and '0's (water), 
+ * count the number of islands. An island is surrounded by water and is formed by connecting adjacent
+ * lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
-Return the number of closed islands.
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
  */
+import java.util.LinkedList;
+import java.util.Queue;
 
-/*
- * NOTE Union Find
+/*0. Iterate from the (0,0) to (M,N), increase res, once encountered a new land
+ *1. Traverse the grid, mark the visited '1' the as '2',put the encountered land to Queue,
+ *2. poll the land from queue and extend to 4 directions, and put the newly encountered land to Queue, 
+ *mark it as '2'
+ *3. repeat 1,2 until queue is empty
  */
 public class NumberofIsland {
-	
-	// exceed time limit
-    public static int numIslands(char[][] grid) {
-        int m=grid.length;
-        if(m==0) return 0;
-        int n=grid[0].length;
-        int key=0;
-        HashMap<Integer, ArrayList<String>> map = new HashMap<Integer,ArrayList<String>>();
-        for(int i=0; i<m;i++) {
-        	for(int j=0;j<n;j++) {
-        		if(i==0 && j==0 && grid[i][j]=='1') {
-        			ArrayList<String> list= new ArrayList<String>();
-        			list.add(String.valueOf(i)+","+String.valueOf(j));
-        			map.put(key, list);
-        		} else if(i==0 && j >0 && grid[i][j]=='1') {
-        			String str=String.valueOf(i)+","+String.valueOf(j-1);
-        			int k=contains(map,str);
-    				str=String.valueOf(i)+","+String.valueOf(j);
-        			if(k==-1) {
-        				key++;
-        				ArrayList<String> nl=new ArrayList<String>();
-        				nl.add(str);
-        				map.put(key,nl);
-        			}else {
-        				map.get(k).add(str);
-        			}
-        		} else if(i > 0 && j==0 && grid[i][j]=='1') {
-        			String str=String.valueOf(i-1)+","+String.valueOf(j);
-        			int k=contains(map,str);
-    				str=String.valueOf(i)+","+String.valueOf(j);
-        			if(k==-1) {
-        				key++;
-        				ArrayList<String> nl=new ArrayList<String>();
-        				nl.add(str);
-        				map.put(key,nl);
-        			}else {
-        				map.get(k).add(str);
-        			}
-        		}
-        		else if(i> 0 && j>0 && grid[i][j]=='1') {
-        			String s1=String.valueOf(i-1)+","+String.valueOf(j);
-        			String s2=String.valueOf(i)+","+String.valueOf(j-1);
-        			int k1=contains(map,s1), k2=contains(map,s2);
-    				s1=String.valueOf(i)+","+String.valueOf(j);
 
-        			if(k1==k2 && k1==-1) {
-        				key++;
-        				ArrayList<String> nl=new ArrayList<String>();
-        				nl.add(s1);
-        				map.put(key,nl);
-        			} else if(k1 != -1 && k2 != -1 && k1 != k2) {
-        				merge(map, k1,k2);
-        				map.get(k1).add(s1);
-        			} else if(k2 != -1) {
-        				map.get(k2).add(s1);
-        			}else if( k1 != -1){
-        				map.get(k1).add(s1);        				
-        			}
-        		}
-        			
-        	}
-        }
-        return map.keySet().size();
-    }
-    public static void merge(HashMap<Integer, ArrayList<String>> map, int k1, int k2) {
-    	for(String str: map.get(k2)) {
-    		map.get(k1).add(str);
-    	}
-    	map.remove(k2);
-    }
-    public static int contains(HashMap<Integer, ArrayList<String>> map, String str) {
-    	int res=-1;
-    	for(int key:  map.keySet()) {
-    		for(String s: map.get(key)) {
-    			if(s.equals(str)) return key;
-    		}
-    	}
-    	return res;
-    }
+	// exceed time limit
+	public static int numIslands(char[][] grid) {
+		int res = 0, m = grid.length;
+		if(m==0) return res;
+		int n = grid[0].length;
+		Queue<int[]> q = new LinkedList<int[]>();
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == '1') {
+					res++;
+					grid[i][j] = '2';
+					int[] index = { i, j };
+					q.add(index);
+					while (!q.isEmpty()) {
+						index = q.poll();
+						extend(q, index[0], index[1], m, n, grid);
+					}
+				}
+			}
+		}
+		return res;
+	}
+
+	public static void extend(Queue<int[]> q, int i, int j, int M, int N, char[][] grid) {
+		// left
+		if (j - 1 >= 0 && grid[i][j - 1] == '1') {q.add(new int[] { i, j - 1 }); grid[i][j-1]='2';}
+		// up
+		if (i - 1 >= 0 && grid[i - 1][j] == '1') {q.add(new int[] { i - 1, j }); grid[i-1][j]='2';}
+		// right
+		if (j + 1 < N && grid[i][j + 1] == '1') {q.add(new int[] { i, j + 1 }); grid[i][j+1]='2';}
+		// down
+		if (i + 1 < M && grid[i + 1][j] == '1') {q.add(new int[] { i + 1, j }); grid[i+1][j]='2';}
+	}
 
 	public static void main(String args[]) {
-		/*
-		 * char [][] grid= {{'1','1','1','1','0'}, {'1','1','0','1','0'},
-		 * {'1','1','0','0','0'}, {'0','0','0','0','0'}};
-		 */
-		char [][]grid= {{'1'}};
-    	System.out.println(numIslands(grid));
-    }
+		char [][] grid= {{'1','1','1','1','0'}, 
+						 {'1','1','0','1','0'},
+						 {'1','1','0','0','0'}, 
+						 {'0','0','0','0','0'}};
+		System.out.println(numIslands(grid));
+	}
 }
