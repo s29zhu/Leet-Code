72594377723a0403369eb1dddac3bc6d3b340127
Shasha <shasha@runwithitsynthetics.com>
2020-07-08 03:50:24 -0600

    the lock problems

---
+++ lock/hard/StrobogrammaticNumberIII.java
@@ -0,0 +1,19 @@
+package leetcode.lock.hard;
+/*
+ * A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to count the total strobogrammatic numbers that exist in the range of low <= num <= high.
+
+Example:
+
+Input: low = "50", high = "100"
+Output: 3 
+Explanation: 69, 88, and 96 are three strobogrammatic numbers.
+Note:
+Because the range might be a large number, the low and high numbers are represented as string.
+ */
+public class StrobogrammaticNumberIII {
+    public int strobogrammaticInRange(String low, String high) {
+        
+    }
+}
